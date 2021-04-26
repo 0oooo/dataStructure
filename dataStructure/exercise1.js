21cc92d9b76431a7f6f89b6681face263e3b3d97
Camille Croci <seh.camille@gmail.com>
2021-04-26 23:48:18 +0100

    Clean the exercise solution

--- dataStructure/exercise1.js
+++ dataStructure/exercise1.js
@@ -1,7 +1,29 @@
-const chars = ['a', 'b', 'c', 'd', 'e'];
+/**
+ * Exercise: 
+ * Given an array of words split into characters, reverse the words in the same array
+ * for example 
+ * const message1 = ['l', 'i', 'e', ' ', 'a', ' ', 'i', 's', ' ', 't', 'h', 'i', 's'];
+ * should become ['t', 'h', 'i', 's', ' ',  'i', 's', ' ', 'a', ' ', l', 'i', 'e', ];
+ */
+
+
+/**
+ * To solve this problem: 
+ * Make a reverse char function 
+ * Reverse the original array and get a totally reversed chars
+ * Loop through the chars and if you find a space or it is the end of the array
+ * Use the reverse char function with the updated index of the beginning of the word 
+ * and the character just before the space 
+ * (or exact character if you've just reached the end)
+ */
+
+ const message = ['l', 'i', 'e', ' ', 
+ 'a', ' ', 
+ 'i', 's', ' ', 
+ 'c', 'a', 'k', 'e', ' ', 
+ 't', 'h', 'e'];
 
 const reverse_chars = (chars, i, j) => {
-
     while(i < j){
         const temp = chars[i]; 
         chars[i] = chars[j]; 
@@ -9,22 +31,15 @@ const reverse_chars = (chars, i, j) => {
         i++; 
         j--; 
     }
+
     return chars; 
 }
-const message1 = ['l', 'i', 'e', ' ', 
-            'a', ' ', 
-            'i', 's', ' ', 
-            'c', 'a', 'k', 'e', ' ', 
-            't', 'h', 'e'];
-
-const message2 = ['a', 'b', ' ', 
-            'i', 's'];
 
 const reverse_word = (message) => {
     let i = 0; 
     let j = message.length - 1; 
     reverse_chars(message, i, j); 
-    
+
     console.log(`Array after inversion ${message}`);
     
     for(let k = 0; k < message.length; k++){
@@ -41,4 +56,4 @@ const reverse_word = (message) => {
     return message; 
 }
 
-reverse_word(message1);
+reverse_word(message);
